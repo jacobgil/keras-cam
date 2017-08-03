dc287f8bb18515cc27018110a7b5db187c2bfb18
Unknown <sdh@eml.cc>
2017-08-03 17:51:01 +1000

    create a simple branch for debugging

--- cam.py
+++ cam.py
@@ -1,15 +1,31 @@
 from keras.models import *
 from keras.callbacks import *
+from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
 import keras.backend as K
 from model import *
 from data import *
-from utils import *
 import cv2
 import argparse
-from keras.utils.np_utils import to_categorical
 BATCH_SIZE = 32
-NB_EPOCHS = 50
+NB_EPOCHS = 40
+
+def get_batches(
+        dirname, 
+        gen=image.ImageDataGenerator(), 
+        shuffle=False, 
+        save_to_dir=None,
+        batch_size=32, 
+        class_mode='categorical',
+        target_size=(128,128)):
+    return gen.flow_from_directory(
+            dirname, 
+            save_to_dir=save_to_dir,
+            target_size=target_size,
+            class_mode=class_mode, 
+            shuffle=shuffle, 
+            batch_size=batch_size)
+
 
 def train(dataset_path):
         gen = ImageDataGenerator(
@@ -41,12 +57,11 @@ def train(dataset_path):
 
 def visualize_class_activation_map(model_path, img_path, output_path):
         model = load_model(model_path)
-        original_img = cv2.imread(img_path, 1)
+        original_img = cv2.resize(cv2.imread(img_path, 1), (224, 224)) 
         width, height, _ = original_img.shape
 
         #Reshape to the network input shape (3, w, h).
         img = np.array([np.transpose(np.float32(original_img), (2, 0, 1))])
-        
         #Get the 512 input weights to the softmax.
         class_weights = model.layers[-1].get_weights()[0]
         final_conv_layer = get_output_layer(model, "conv5_3")
@@ -54,16 +69,16 @@ def visualize_class_activation_map(model_path, img_path, output_path):
         [conv_outputs, predictions] = get_output([img])
         conv_outputs = conv_outputs[0, :, :, :]
 
-        print "predictions", predictions
-        target_class = 0
         #Create the class activation map.
         cam = np.zeros(dtype = np.float32, shape = conv_outputs.shape[1:3])
-        for i, w in enumerate(class_weights[:, target_class]):
+        for i, w in enumerate(class_weights[:, 1]):
                 cam += w * conv_outputs[i, :, :]
+        print "predictions", predictions
         cam /= np.max(cam)
         cam = cv2.resize(cam, (height, width))
         heatmap = cv2.applyColorMap(np.uint8(255*cam), cv2.COLORMAP_JET)
         heatmap[np.where(cam < 0.2)] = 0
+        print(heatmap.shape)
         img = heatmap*0.5 + original_img
         cv2.imwrite(output_path, img)
 
--- data.py
+++ data.py
@@ -1,58 +1,16 @@
 import cv2
-
 import glob
 import os
 import numpy as np
 from keras.utils.np_utils import to_categorical
-from preprocessing import preprocess_image_batch
-
-CLASS_NAME_MAPPING = {}
-PER_CLASS_MAX_IMAGES = 24
-
-
-def load_data(path): 
-    data_set_input_images_files, data_set_input_images_true_label = get_class_wise_images_and_true_label(path)
-    processed_input_images = [preprocess_image_batch([image])
-                               for image in data_set_input_images_files]
-
-    global CLASS_NAME_MAPPING
-    nb_classes = len(CLASS_NAME_MAPPING.keys())
-    X_train = np.concatenate(processed_input_images)
-
-    y_out = np.concatenate(data_set_input_images_true_label)
-    y_out = to_categorical(y_out, nb_classes=nb_classes)  # to get sofmax shape of (None, nb_classes)
-    Y_train = y_out
-
-
-    from sklearn.utils import shuffle
-    X_train, Y_train = shuffle(X_train, Y_train)
-
-    return X_train, Y_train, nb_classes
-
-
-def get_class_wise_images_and_true_label(path):
-    print('path', path+"/*")
-    directory = glob.glob(path + '/*')
-    data_set_input_images = []
-    data_set_input_images_true_label = []
-    global CLASS_NAME_MAPPING
-    index = 0
-    for sub_directory in directory:
-        if os.path.isdir(sub_directory):
-            class_dir_name = sub_directory.split('/')[-1]
-            CLASS_NAME_MAPPING[index] = class_dir_name
-            image_class_files = glob.glob(sub_directory + '/*.jpeg')[:PER_CLASS_MAX_IMAGES]
-            data_set_input_images.extend(image_class_files)
-            data_set_input_images_true_label.extend([[index]] * len(image_class_files))
-            index += 1
-    return data_set_input_images, data_set_input_images_true_label
 
 def load_inria_person(path):
-    pos_path = os.path.join(path, "pos")
-    neg_path = os.path.join(path, "neg")
-    pos_images = [cv2.resize(cv2.imread(x), (64, 128)) for x in glob.glob(pos_path + "/*.jpeg")]
+    pos_path = "./data/train/pos"
+    neg_path = "./data/train/neg"
+    pos_images = [cv2.resize(cv2.imread(x), (128, 128)) for x in glob.glob(pos_path + "/*.jpg")]
     pos_images = [np.transpose(img, (2, 0, 1)) for img in pos_images]
-    neg_images = [cv2.resize(cv2.imread(x), (64, 128)) for x in glob.glob(neg_path + "/*.jpeg")]
+    neg_images = [cv2.resize(cv2.imread(x), (128, 128)) for x in glob.glob(neg_path + "/*.jpg")]
+    print(pos_path, neg_path)
     neg_images = [np.transpose(img, (2, 0, 1)) for img in neg_images]
     y = [1] * len(pos_images) + [0] * len(neg_images)
     y = to_categorical(y, 2)
